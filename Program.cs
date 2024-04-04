75bd3dbe647c02faaa2db030ea8cb0d6ab0029ef
Muhammad Mouiz Ghouri <mouizghouri77@gmail.com>
2024-04-04 20:53:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿public class NumbersFact
+{
+    public static async Task Main()
+    {
+        Console.WriteLine(">> This program gives you a random fact about any day of the year. ");
+
+        Console.Write("Enter the number of month (1-12): ");
+        int month = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter the number of day: ");
+        int day = int.Parse(Console.ReadLine());
+
+        // Write code below this line
+
+        // Write code above this line
+    }
+}
