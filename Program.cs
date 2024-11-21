5fdb408aaec70d7aebd484fecffd9c5a8f5dd59e
James Flynn <jamesdeclonflynn@hotmail.com>
2024-11-21 09:58:22 +0000

    Added try catch to make sure it is safely running

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
-﻿public class NumbersFact
+﻿using System.Security.Authentication.ExtendedProtection;
+
+public class NumbersFact
 {
-    public static async Task Main()
+    static async Task Main()
     {
         Console.WriteLine(">> This program gives you a random fact about any day of the year. ");
 
@@ -11,7 +13,26 @@
         int day = int.Parse(Console.ReadLine());
 
         // Write code below this line
+        try
+        {
+            string url = $"http://numbersapi.com/{month}/{day}/date";
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(url);
 
+            if (response.IsSuccessStatusCode)
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                Console.WriteLine("Fact: " + responseContent);
+            }
+            else
+            {
+                Console.WriteLine($"API request failed: {response.StatusCode}");
+            }
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
         // Write code above this line
     }
-}
+} 
